82a4e4f6a73c8931eb5fbd741c9f4bd9784a5ded
Clyde Jones <81191399+helix-clyde@users.noreply.github.com>
2021-08-17 16:05:11 -0700

    scripts for manual launch

---
+++ nodeexporter.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -x
+
+export VERSION="v1.2.0"
+
+docker run \
+  --name node-exporter \
+  --restart unless-stopped \
+  -p 9100:9100 \
+  -d \
+  -v /:/host:ro \
+  quay.io/prometheus/node-exporter:${VERSION} \
+    --path.rootfs=/host
---
+++ promstart.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+[[ $DEBUG ]] && set -x
+
+PROM_BASE="/opt/prometheus"
+export VERSION="v2.29.1"
+
+mkdir -vp ${PROM_BASE}/etc/ \
+          ${PROM_BASE}/data/ \
+          ${PROM_BASE}/log/
+
+docker run \
+        --name=prometheus \
+        -d \
+        --restart unless-stopped \
+        -p 9090:9090 \
+        -v ${PROM_BASE}/data/:/var/lib/prometheus/data/ \
+        -v ${PROM_BASE}/etc/:/etc/prometheus/ \
+        -v ${PROM_BASE}/log/:/var/log/prometheus/ \
+        quay.io/prometheus/prometheus:${VERSION} \
+            --config.file=/etc/prometheus/prometheus.yml \
+            --storage.tsdb.path="/var/lib/prometheus/data/"
