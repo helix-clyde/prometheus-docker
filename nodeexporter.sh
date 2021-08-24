63195e5d1e7dbc212e54537037d67892784046b0
Clyde Jones <clyde.jones@helix.com>
2021-08-23 19:23:31 -0700

    force health check into launch

--- nodeexporter.sh
+++ nodeexporter.sh
@@ -9,6 +9,9 @@ docker run \
   --restart unless-stopped \
   -p 9100:9100 \
   -d \
+  --health-cmd='curl -s --fail http://localhost:9100 -o /dev/null' \
+  --health-interval=30s \
+  --health-retries=3 \
   -v /:/host:ro \
   quay.io/prometheus/node-exporter:${VERSION} \
     --path.rootfs=/host \
