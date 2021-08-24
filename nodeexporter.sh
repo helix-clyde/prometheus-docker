61f92c15cd8e983ac874cdc957e8287c370c01c6
Clyde Jones <clyde.jones@helix.com>
2021-08-24 10:30:46 -0700

    fix health check

--- nodeexporter.sh
+++ nodeexporter.sh
@@ -7,11 +7,11 @@ export VERSION="v1.2.0"
 docker run \
   --name node-exporter \
   --restart unless-stopped \
-  -p 9100:9100 \
-  -d \
-  --health-cmd='curl -s --fail http://localhost:9100 -o /dev/null' \
+  --health-cmd='wget -q --spider https://localhost:9100/metrics' \
   --health-interval=30s \
   --health-retries=3 \
+  -p 9100:9100 \
+  -d \
   -v /:/host:ro \
   quay.io/prometheus/node-exporter:${VERSION} \
     --path.rootfs=/host \
