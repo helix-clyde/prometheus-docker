facaf0d498093c33a96a1c3de200b7c74e062b83
Clyde Jones <clyde.jones@helix.com>
2021-08-24 21:21:14 -0700

    add execution check

--- nodeexporter.sh
+++ nodeexporter.sh
@@ -4,16 +4,18 @@ set -x
 
 export VERSION="v1.2.0"
 
-docker run \
-  --name node-exporter \
-  --restart unless-stopped \
-  --health-cmd='wget -q --spider http://localhost:9100/metrics' \
-  --health-interval=30s \
-  --health-retries=3 \
-  -p 9100:9100 \
-  -d \
-  -v /:/host:ro \
-  quay.io/prometheus/node-exporter:${VERSION} \
-    --path.rootfs=/host \
-    --collector.ntp \
-    --collector.supervisord
+if [[ $(docker ps  | grep node-exporter | wc -l| tr -d ' ') -ne "1" ]]
+  docker run \
+    --name node-exporter \
+    --restart unless-stopped \
+    --health-cmd='wget -q --spider http://localhost:9100/metrics' \
+    --health-interval=30s \
+    --health-retries=3 \
+    -p 9100:9100 \
+    -d \
+    -v /:/host:ro \
+    quay.io/prometheus/node-exporter:${VERSION} \
+      --path.rootfs=/host \
+      --collector.ntp \
+      --collector.supervisord
+fi
