9caa8c3195764d5dc2870a6b2126c407e4e4bf25
Clyde Jones <clyde.jones@helix.com>
2021-08-30 16:49:40 -0700

    explicit ref to config dir

--- etc/prometheus/targets/configs.sh
+++ etc/prometheus/targets/configs.sh
@@ -8,16 +8,16 @@ MASTERNODE=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 for node in $(cat ~clyde.jones/nodes.txt | grep -v $MASTERNODE); do
   echo $node
   if [[ $(curl --connect-timeout 1 --fail http://$x:9100/metrics -o /dev/null) ]]; then
-      /bin/cp -v node.yml.tmpl $node.yml \
-      && sed -i -e "s/X.X.X.X/$node/" $node.yml
+      /bin/cp -v $TARGET_DIR/node.yml.tmpl $TARGET_DIR/$node.yml \
+      && sed -i -e "s/X.X.X.X/$node/" $TARGET_DIR/$node.yml
   fi
 done
 
 cd $TARGET_DIR
 
-for x in $(cat *.yml | grep 9100 | cut -d \' -f 2 | cut -d ":" -f 1 ); do
+for x in $(cat $TARGET_DIR/*.yml | grep 9100 | cut -d \' -f 2 | cut -d ":" -f 1 ); do
     curl --connect-timeout 2 \
               --fail http://$x:9100/metrics \
               -o /dev/null \
-    || mv -v $x.yml $x.yml.old
+    || mv -v $TARGET_DIR/$x.yml $TARGET_DIR/$x.yml.old
  done
