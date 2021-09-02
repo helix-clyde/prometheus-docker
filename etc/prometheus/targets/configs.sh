0945dc792420c5ce1b31310812a816d2a3ecd462
Clyde Jones <clyde.jones@helix.com>
2021-09-02 14:19:23 -0700

    change to silent curl

--- etc/prometheus/targets/configs.sh
+++ etc/prometheus/targets/configs.sh
@@ -4,10 +4,11 @@ set -x
 
 TARGET_DIR=/efs/monitoring/prometheus/etc/targets/
 MASTERNODE=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+NWMASK=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 | cut -d . -f1,2)
 
 for node in $(cat ~clyde.jones/nodes.txt | grep -v $MASTERNODE); do
   echo $node
-  if [[ $(curl --connect-timeout 1 --fail http://$x:9100/metrics -o /dev/null) ]]; then
+  if [[ $(curl -s --connect-timeout 1 --fail http://$x:9100/metrics -o /dev/null) ]]; then
       /bin/cp -v $TARGET_DIR/node.yml.tmpl $TARGET_DIR/$node.yml \
       && sed -i -e "s/X.X.X.X/$node/" $TARGET_DIR/$node.yml
   fi
@@ -17,7 +18,8 @@ cd $TARGET_DIR
 
 for x in $(cat $TARGET_DIR/*.yml | grep 9100 | cut -d \' -f 2 | cut -d ":" -f 1 ); do
     curl --connect-timeout 2 \
-              --fail http://$x:9100/metrics \
-              -o /dev/null \
+         -s \
+         --fail http://$x:9100/metrics \
+         -o /dev/null \
     || mv -v $TARGET_DIR/$x.yml $TARGET_DIR/$x.yml.old
  done
