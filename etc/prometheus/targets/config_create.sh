ba6016280f4a566e7b923468a9f00432149cb3df
Clyde Jones <clyde.jones@helix.com>
2021-09-09 14:52:12 -0700

    script for node to create it's own configuration

---
+++ etc/prometheus/targets/config_create.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# for a node to create it's own configuration
+set -x
+
+TARGET_DIR=/efs/monitoring/prometheus/etc/targets/
+NODEIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
+sudo /bin/cp -v $TARGET_DIR/node.yml.tmpl $TARGET_DIR/${NODEIP}.yml \
+&& sudo sed -i -e "s/X.X.X.X/${NODEIP}/" $TARGET_DIR/${NODEIP}.yml
