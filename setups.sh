baac2231257a4678e3c1a47916c35c92733e921f
Clyde Jones <clyde.jones@helix.com>
2021-09-09 14:55:41 -0700

    parameterize config location add call to local config creation

--- setups.sh
+++ setups.sh
@@ -2,7 +2,8 @@
 
 # set -x
 
-rm -v ~/nodes.txt
+NODELIST=~/nodes.txt
+rm -v $NODELIST
 
 for host in $(qconf -sh | sort -R ); do
   (ssh \
@@ -10,20 +11,12 @@ for host in $(qconf -sh | sort -R ); do
       -o ConnectTimeout=1 \
       $host \
        "sudo ~clyde.jones/prometheus-docker/nodeexporter.sh \
-       ; ~clyde.jones/bin/getip.sh") \
+       ; ~clyde.jones/bin/getip.sh \
+       ; ~clyde.jones/prometheus-docker/etc/prometheus/targets/config_create.sh"\
+  ) \
    | grep 172\. \
-   |tee -a ~/nodes.txt
+   |tee -a ${NODELIST} \
+
 done
        # sudo docker kill node-exporter; \
        # sudo docker rm node-exporter; \
-
-# for node in $(cat ~clyde.jones/nodes.txt| grep -v 172.30.66.60); do
-#   echo $node ; /bin/cp -v node.yml.tmpl $node.yml ; sed -i -e "s/X.X.X.X/$node/" $node.yml\
-#   ; sleep 1
-# done
-#
-# for x in $(cat *.yml | grep 9100 | cut -d \' -f 2 | cut -d ":" -f 1 ); do
-#    echo $x \
-#    ; curl --connect-timeout 2 \
-#           --fail http://$x:9100/metrics 2>&1 > /dev/null \
-#    || mv -v $x.yml $x.yml.old ; done
