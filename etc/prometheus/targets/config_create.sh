04820790a08087d9535462c1974a2aa4edfef77a
Clyde Jones <clyde.jones@helix.com>
2021-09-09 14:56:02 -0700

    only create config if does not exist

--- etc/prometheus/targets/config_create.sh
+++ etc/prometheus/targets/config_create.sh
@@ -6,5 +6,7 @@ set -x
 TARGET_DIR=/efs/monitoring/prometheus/etc/targets/
 NODEIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
-sudo /bin/cp -v $TARGET_DIR/node.yml.tmpl $TARGET_DIR/${NODEIP}.yml \
-&& sudo sed -i -e "s/X.X.X.X/${NODEIP}/" $TARGET_DIR/${NODEIP}.yml
+if [[ ! -e $TARGET_DIR/${NODEIP}.yml ]]; then
+  sudo /bin/cp -v $TARGET_DIR/node.yml.tmpl $TARGET_DIR/${NODEIP}.yml \
+  && sudo sed -i -e "s/X.X.X.X/${NODEIP}/" $TARGET_DIR/${NODEIP}.yml
+fi
