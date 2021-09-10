e183c4eb9ac93c21c50db69e63a1371e230d7a86
Clyde Jones <81191399+helix-clyde@users.noreply.github.com>
2021-09-09 17:13:11 -0700

    added parallel switch

--- setups.sh
+++ setups.sh
@@ -5,18 +5,30 @@
 NODELIST=~/nodes.txt
 rm -v $NODELIST
 
-for host in $(qconf -sh | sort -R ); do
-  (ssh \
+if [[ $(type -p parallel) ]]; then
+  qconf -sh \
+  | sort -R \
+  | parallel -j 10 ssh \
       -o StrictHostKeyChecking=false \
       -o ConnectTimeout=1 \
       $host \
        "sudo ~clyde.jones/prometheus-docker/nodeexporter.sh \
-       ; ~clyde.jones/prometheus-docker/etc/prometheus/targets/config_create.sh \
-       ; ~clyde.jones/bin/getip.sh" \
-  ) \
-   | grep 172\. \
-   |tee -a ${NODELIST} \
+       ; ~clyde.jones/prometheus-docker/etc/prometheus/targets/config_create.sh"
+else
+  for host in $(qconf -sh | sort -R ); do
+    (ssh \
+        -o StrictHostKeyChecking=false \
+        -o ConnectTimeout=1 \
+        $host \
+         "sudo ~clyde.jones/prometheus-docker/nodeexporter.sh \
+         ; ~clyde.jones/prometheus-docker/etc/prometheus/targets/config_create.sh \
+         ; ~clyde.jones/bin/getip.sh" \
+    ) \
+     | grep 172\. \
+     |tee -a ${NODELIST} \
 
-done
-       # sudo docker kill node-exporter; \
-       # sudo docker rm node-exporter; \
+  done
+fi
+
+# sudo docker kill node-exporter; \
+# sudo docker rm node-exporter; \
