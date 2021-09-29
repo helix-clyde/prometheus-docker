38fa2ec97bd68dcebdd1c6623232cb657758709f
Clyde Jones <81191399+helix-clyde@users.noreply.github.com>
2021-09-29 11:53:21 -0700

    remove redundant code

--- etc/prometheus/targets/configs.sh
+++ etc/prometheus/targets/configs.sh
@@ -6,21 +6,20 @@ TARGET_DIR=/efs/monitoring/prometheus/etc/targets/
 MASTERNODE=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 NWMASK=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 | cut -d . -f1,2)
 
-for node in $(cat ~clyde.jones/nodes.txt | grep -v $MASTERNODE); do
-  echo $node
-  # if [[ $(curl -s --connect-timeout 1 --fail http://${node}:9100/metrics -o /dev/null) ]]; then
-  if [[ ! -e $TARGET_DIR/${node}.yml ]]; then
-      /bin/cp -v $TARGET_DIR/node.yml.tmpl $TARGET_DIR/$node.yml \
-      && sed -i -e "s/X.X.X.X/$node/" $TARGET_DIR/$node.yml
-  fi
-done
+# for node in $(cat ~clyde.jones/nodes.txt | grep -v $MASTERNODE); do
+#   echo $node
+#       /bin/cp -v $TARGET_DIR/node.yml.tmpl $TARGET_DIR/$node.yml \
+#       && sed -i -e "s/X.X.X.X/$node/" $TARGET_DIR/$node.yml
+# done
 
 cd $TARGET_DIR
+mkdir -vp ${TARGET_DIR}/old/
 
 for x in $(cat $TARGET_DIR/*.yml | grep 9100 | cut -d \' -f 2 | cut -d ":" -f 1 ); do
     curl --connect-timeout 2 \
          -s \
          --fail http://$x:9100/metrics \
          -o /dev/null \
-    || mv -v $TARGET_DIR/$x.yml $TARGET_DIR/$x.yml.old
+    || mv -v $TARGET_DIR/$x.yml $TARGET_DIR/old/$x.yml.old
  done
+
