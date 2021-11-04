64817f048191c2f0fdee295a7d65ca1c39c9d924
Clyde Jones <clyde.jones@helix.com>
2021-11-03 17:01:28 -0700

    set up to do dependent jobs

--- qsub.sh
+++ qsub.sh
@@ -2,12 +2,36 @@
 
 set -x
 
+# qsub \
+#   -q ondemand.q \
+#   -hold_jid 32052 \
+#   -N ne-setup \
+#   -o /efs/home/clyde.jones/logs/prometheus-docker/submits-$(date +%F).out \
+#   -e /efs/home/clyde.jones/logs/prometheus-docker/submits-$(date +%F).err < ~/prometheus-docker/submit.sh
+
 LOGDIR=/efs/home/clyde.jones/logs/prometheus-docker
+
 mkdir -vp $LOGDIR/
 
-qsub \
-    -q ondemand.q \
-    -N node_exporter \
-    -o $LOGDIR/prom-setup-$(date +%F-%H%M).out \
-    -e $LOGDIR/prom-setup-$(date +%F-%H%M).err \
-    < ~/prometheus-docker/setups.sh 
+JOB_ID = ""
+for (( i = 0; i < 10; i++ )); do
+  if [[ ${JOB_ID} == "" ]]; then
+    JOB_ID=$(qsub \
+        -q ondemand.q \
+        -terse \
+        -N node_ex \
+        -o $LOGDIR/prom-setup-$(date +%F-%H%M).out \
+        -e $LOGDIR/prom-setup-$(date +%F-%H%M).err \
+        < ~/prometheus-docker/setups.sh )
+  else
+    JOB_ID=$(qsub \
+        -q ondemand.q \
+        -terse \
+        -N node_ex_${JOB_ID} \
+        -hold_jid $JOB_ID\
+        -o $LOGDIR/prom-setup-$(date +%F-%H%M).out \
+        -e $LOGDIR/prom-setup-$(date +%F-%H%M).err \
+        < ~/prometheus-docker/setups.sh )
+  fi
+  echo $JOB_ID
+done
