8e6186f6f9b9f7f6b35c1fe3fb3f56402faac695
Clyde Jones <clyde.jones@helix.com>
2022-02-28 14:53:27 -0800

    added health check and and option to allow web forced reloads

--- promstart.sh
+++ promstart.sh
@@ -19,12 +19,16 @@ docker run \
         -d \
         --restart unless-stopped \
         -p 9090:9090 \
+        --health-cmd='/bin/wget -q --spider http://localhost:9090/' \
+        --health-interval=30s \
+        --health-retries=3 \
         -v ${PROM_BASE}/data/:/var/lib/prometheus/data/ \
         -v ${EFS_BASE}/etc/:/etc/prometheus/ \
         -v ${PROM_BASE}/log/:/var/log/prometheus/ \
         quay.io/prometheus/prometheus:${VERSION} \
             --config.file=/etc/prometheus/prometheus.yml \
             --storage.tsdb.path="/var/lib/prometheus/data/" \
+            --storage.tsdb.retention.size=1GB \
             --storage.tsdb.retention.time=7d \
             --storage.tsdb.wal-compression \
-            --storage.tsdb.retention.size=1GB
+            --web.enable-lifecycle
