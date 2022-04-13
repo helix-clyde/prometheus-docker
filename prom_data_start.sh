7dc65cb5c496d74a1cde6f83592f67e823fa9748
Clyde Jones <81191399+helix-clyde@users.noreply.github.com>
2022-04-13 10:42:57 -0700

    updated paths

--- prom_data_start.sh
+++ prom_data_start.sh
@@ -4,7 +4,8 @@
 
 NODEIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 PROM_BASE="/opt/prometheus"
-EFS_BASE="/scratch/prometheus"
+# EFS_BASE="/scratch/prometheus"
+EFS_BASE="/efs/monitoring/prometheus"
 PROM_VOL="prometheus_data"
 
 export VERSION="v2.34.0"
@@ -45,7 +46,7 @@ docker run \
         prom/prometheus:${VERSION} \
             --config.file=/etc/prometheus/prometheus.yml \
             --storage.tsdb.path="/var/lib/prometheus/data/" \
-            --storage.tsdb.retention.size=5GB \
+            --storage.tsdb.retention.size=2GB \
             --storage.tsdb.retention.time=14d \
             --storage.tsdb.wal-compression \
             --web.enable-lifecycle
