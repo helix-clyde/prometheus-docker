9ec8e179aaa49e26004eaaa87a31348b9edbe70b
Clyde Jones <81191399+helix-clyde@users.noreply.github.com>
2022-08-04 11:07:47 -0700

    change to use hostname as config

--- etc/prometheus/targets/config_create.sh
+++ etc/prometheus/targets/config_create.sh
@@ -5,10 +5,11 @@ set -x
 
 TARGET_DIR=/efs/monitoring/prometheus/etc/targets
 NODEIP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+HOSTNAME=${HOSTNAME=:$(curl -s http://169.254.169.254/latest/meta-data/hostname | cut -d . -f 1)}
 
-if [[ ! -e $TARGET_DIR/${NODEIP}.yml ]]; then
-  sudo /bin/cp -v $TARGET_DIR/node.yml.tmpl $TARGET_DIR/${NODEIP}.yml \
-  && sudo sed -i -e "s/X.X.X.X/${NODEIP}/" $TARGET_DIR/${NODEIP}.yml
+if [[ ! -e "$TARGET_DIR"/"$HOSTNAME".yml ]]; then
+    sudo /bin/cp -v "$TARGET_DIR/node.yml.tmpl" "$TARGET_DIR/$HOSTNAME".yml \
+ && sudo sed -i -e "s/X.X.X.X/$NODEIP/" "$TARGET_DIR/$HOSTNAME".yml
 fi
 
 # End of file, if this is missing the file is truncated
