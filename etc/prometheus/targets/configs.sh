9077f8b6ab46215dca97e0222711e02f8ecfcb78
Clyde Jones <81191399+helix-clyde@users.noreply.github.com>
2022-08-09 18:04:10 -0700

    remove unused variables

--- etc/prometheus/targets/configs.sh
+++ etc/prometheus/targets/configs.sh
@@ -3,8 +3,6 @@
 set -x
 
 TARGET_DIR=/efs/monitoring/prometheus/etc/targets
-MASTERNODE=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-NWMASK=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 | cut -d . -f1,2)
 
 cd $TARGET_DIR
 mkdir -vp ${TARGET_DIR}/old/
