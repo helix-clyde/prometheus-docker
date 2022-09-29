4de618472906ab5b2ba926c1b1576e8b6cd7ce1c
Clyde Jones <81191399+helix-clyde@users.noreply.github.com>
2022-09-28 20:42:15 -0700

    add debug for set -x and test for location

--- etc/prometheus/targets/config_create.sh
+++ etc/prometheus/targets/config_create.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # for a node to create it's own configuration
-set -x
+[[ $DEBUG ]] && set -x
 
 TARGET_DIR=/efs/monitoring/prometheus/etc/targets
 NODEIP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
@@ -10,6 +10,8 @@ HOSTNAME=${HOSTNAME=:$(curl -s http://169.254.169.254/latest/meta-data/hostname 
 if [[ ! -e "$TARGET_DIR"/"$HOSTNAME".yml ]]; then
     sudo /bin/cp -v "$TARGET_DIR/node.yml.tmpl" "$TARGET_DIR/$HOSTNAME".yml \
  && sudo sed -i -e "s/X.X.X.X/$NODEIP/" "$TARGET_DIR/$HOSTNAME".yml
+else 
+    echo "$TARGET_DIR/$HOSTNAME.yml there"
 fi
 
 # End of file, if this is missing the file is truncated
